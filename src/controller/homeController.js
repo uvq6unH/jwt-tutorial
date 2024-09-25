6290f970e97cd7fb2e6b9982e3effa9fede37b03
Josee <xenonex04@gmail.com>
2024-09-26 02:14:34 +0700

    create new user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,10 +1,33 @@
-const handleHelloWord = (req, res)=> {
+import mysql from 'mysql2';
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt'
+});
+
+const handleHelloWord = (req, res) => {
     return res.render("home.ejs");
 }
-const handleUserPage = (req, res)=> {
+const handleUserPage = (req, res) => {
     return res.render("user.ejs");
+}
+const handleCreateNewUser = (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let username = req.body.username;
+
+    connection.query(
+        'INSERT INTO users (email, password, username) VALUES (?, ?, ?) ',[email, password, username],
+        function (err, results, fields) {
+            if (err) {
+                console.log(err);   
+            }
+        }
+    );
+
+    return res.send("handleCreateNewUser");
 }
 
 module.exports = {
-    handleHelloWord,handleUserPage
+    handleHelloWord, handleUserPage, handleCreateNewUser
 }
