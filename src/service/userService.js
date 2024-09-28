baaf4a8793ae205b97cd2716fe8ef8e3882744ba
Josee <xenonex04@gmail.com>
2024-09-28 17:36:16 +0700

    ORM relationships

--- src/service/userService.js
+++ src/service/userService.js
@@ -21,6 +21,23 @@ const createNewUser = async (email, password, username) => {
     }
 }
 const getUserList = async () => {
+
+    let newUser = await db.User.findOne({
+        where: { id: 1 },
+        attributes: ["id", "username", "email"],
+        include: { model: db.Group, attributes: [ "name", "description"], },
+        raw: true,
+        nest: true
+    })
+    console.log(">>> check new users: ", newUser)
+
+    let r = await db.Role.findAll({
+        include: { model: db.Group, where: { id: 1 } },
+        raw: true,
+        nest: true
+    })
+    console.log(">>> check new g: ", r)
+
     let users = [];
     users = await db.User.findAll();
     return users;
