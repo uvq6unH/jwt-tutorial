b1db79ae03bf2487ec08b7b3c15e0e55d1814341
Josee <xenonex04@gmail.com>
2024-10-08 17:02:23 +0700

    api create new user

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,11 +1,39 @@
+import loginRegisterService from '../service/loginRegisterService';
 const testApi = (req, res) => {
     return res.status(200).json({
         message: 'ok',
         data: 'test api'
     })
 }
-const handleRegister = (req, res) => {
-    console.log("call me",req.body)
+const handleRegister = async (req, res) => {
+    try {
+        if (!req.body.email || !req.body.phone || !req.body.password) {
+            return res.status(200).json({
+                EM: 'Missing required parameters ',
+                EC: '1',
+                DT: ''
+            })
+        }
+        if(req.body.password && req.body.password.length < 8 ){
+            return res.status(200).json({
+                EM: 'Your password must have more than 8 letters ',
+                EC: '1',
+                DT: ''
+            })
+        }
+        let data = await loginRegisterService.registerNewUser(req.body)
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: ''
+        })
+    } catch (e) {
+        return res.status(500).json({
+            EM: 'error from server ',
+            EC: '-1',
+            DT: ''
+        })
+    }
 }
 module.exports = {
     testApi, handleRegister
